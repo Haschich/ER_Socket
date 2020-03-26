0310c417997e19482a2fe92dab287ba1791b7f29
Gustavo Dois <g.haschich@gmail.com>
2020-03-26 11:57:24 -0300

    long polling support

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-// var express = require("express");
-// var app = new express();
-// var ip = "http";
-// var http = require(ip).Server(app);
-// var io = require("socket.io")(http);
-// 
-// var port = process.env.PORT || 80;
-// 
-// app.use(express.static(__dirname + "/public" ));
-// 
-// app.get('/',function(req,res){
-// res.redirect('public/index.html');
-// });
-// 
-// io.on('connection',function(socket){
-// 
-//     socket.on('stream',function(image){
-//         socket.broadcast.emit('stream',image);  
-//     });
-// 
-// });
-// 
-// http.listen(port,function(){
-// console.log("Server running at port "+ port);
-// });
-
 var express = require("express");
 var app = new express();
 var ip = "http";
 var http = require(ip).Server(app);
-var io = require("socket.io")(http, {
-    transports: ['websocket'],
-    serveClient: false,
-});
+var io = require("socket.io")(http);
 
 var port = process.env.PORT || 80;
 
 app.use(express.static(__dirname + "/public" ));
 
 app.get('/',function(req,res){
-    res.redirect('public/index.html');
+res.redirect('public/index.html');
 });
 
-
 io.on('connection',function(socket){
-    socket.on('reconnect_attempt', () => {
-        socket.io.opts.transports = ['websocket'];
-    });
+
     socket.on('stream',function(image){
-        socket.broadcast.emit('stream',image);
+        socket.broadcast.emit('stream',image);  
     });
 
 });
 
 http.listen(port,function(){
-    console.log("Server running at port "+ port);
+console.log("Server running at port "+ port);
 });
