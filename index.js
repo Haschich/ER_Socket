90ed741fafcc70b54b8185ee7ef91fb26dd2b8fa
Gustavo Dois <g.haschich@gmail.com>
2020-03-26 13:02:22 -0300

    removed lp

--- index.js
+++ index.js
@@ -6,28 +6,17 @@ var port = process.env.PORT || 80;
 
 app.listen(port);
 
-
-// http.listen(port,function(){
-//     console.log("Server running at port "+ port);
-// });
-// app.use(express.static(__dirname + "/public" ));
-//
-// app.get('/',function(req,res){
-//     res.redirect('public/index.html');
-// });
-
-
 function handler (req, res) {
+    console.log("Server open at port "+ port);
     fs.readFile(__dirname + '/index.html',
         function (err, data) {
             if (err) {
                 res.writeHead(500);
                 return res.end('Error loading index.html');
             }
-
             res.writeHead(200);
             res.end(data);
-            console.log("Server running at port "+ port);
+            console.log("Connection");
         });
 }
 
