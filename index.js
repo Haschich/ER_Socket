a2a94bb712aef204e5f9988399348aecc267de7b
Gustavo Dois <g.haschich@gmail.com>
2020-03-26 11:13:28 -0300

    Forcing websocket connection

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require("express");
 var app = new express();
 var ip = "http";
 var http = require(ip).Server(app);
-var io = require("socket.io")(http);
+var io = require("socket.io")(http, {
+    transports: ['websocket'],
+    serveClient: false,
+});
  
 var port = process.env.PORT || 7777;
  
@@ -14,7 +17,9 @@ res.redirect('public/index.html');
  
  
 io.on('connection',function(socket){
- 
+    socket.on('reconnect_attempt', () => {
+        socket.io.opts.transports = ['websocket'];
+    });
     socket.on('stream',function(image){
         socket.broadcast.emit('stream',image);  
     });
