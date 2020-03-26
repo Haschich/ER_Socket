4c8d6fb7247fde1f2838368c4d7b43ee0ab5c608
Gustavo Dois <g.haschich@gmail.com>
2020-03-26 12:38:43 -0300

    removed express

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-var express = require("express");
-var app = new express();
-var ip = "http";
-var http = require(ip).Server(app);
-var io = require("socket.io")(http);
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
 var port = process.env.PORT || 80;
 
-http.listen(port,function(){
-    console.log("Server running at port "+ port);
-});
+app.listen(port);
 
-app.use(express.static(__dirname + "/public" ));
 
-app.get('/',function(req,res){
-    res.redirect('public/index.html');
-});
+// http.listen(port,function(){
+//     console.log("Server running at port "+ port);
+// });
+// app.use(express.static(__dirname + "/public" ));
+//
+// app.get('/',function(req,res){
+//     res.redirect('public/index.html');
+// });
+
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/public/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+            console.log("Server running at port "+ port);
+        });
+}
 
 io.on('connection',function(socket){
     socket.on('stream',function(image){
         socket.broadcast.emit('stream',image);  
     });
-
 });
 
 
