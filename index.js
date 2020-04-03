32c54e97ccd34ab92122e1fbce8c3d8de48b1526
Gustavo Dois <g.haschich@gmail.com>
2020-04-03 12:27:09 -0300

    Update 1

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-var express = require("express");
-var app = new express();
-var ip = "http";
-var http = require(ip).Server(app);
-var io = require("socket.io")(http);
-var port = process.env.PORT || 80;
- 
-app.use(express.static(__dirname));
- 
+// var express = require("express");
+// var app = new express();
+// var ip = "http";
+// var http = require(ip).Server(app);
+// var io = require("socket.io")(http);
+// var port = process.env.PORT || 80;
+// 
+// app.use(express.static(__dirname));
+// 
+
+const express    = require('express');
+const path       = require('path');
+const port       = process.env.PORT || 80;
+const app        = express();
+
+/**
+ * WebSocket Configuration
+ */
+const io = require('socket.io')(port, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin,
+            "Access-Control-Allow-Credentials": true,
+        };
+        res.writeHead(200, headers);
+        res.end();
+    },
+    path: '/',
+    serveClient: true,
+    origins: '*:*',
+    cookie: true,
+    pingInterval: 1000,
+    pingTimeout: 1000,
+    upgradeTimeout: 1000,
+    allowUpgrades: true,
+    cookie: 'remote_spec',
+    cookiePath:'/',
+    cookieHttpOnly:true
+});
+    
+
+
+
+
+
 app.get('/',function(req,res){
 res.redirect('index.html');
 });
- 
  
 io.on('connection',function(socket){
     socket.on('stream',function(image){
         socket.broadcast.emit('stream',image);  
     });
 });
- 
-http.listen(port,function(){
-console.log("Server running at port "+ port);
-});
+
+app.listen(port, () => console.log("Server running at port "+ port);));
+
+// http.listen(port,function(){
+// console.log("Server running at port "+ port);
+// });
